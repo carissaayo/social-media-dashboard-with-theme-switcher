1c798953f83fad8e39634cd70351f7c4cf6afc48
CarissaAyo <ajaoyussufayo@gmail.com>
2021-05-04 07:24:45 +0100

    remains making it responsive on mobiles and tablets

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+const toggle = document.querySelector("#toggle-btn");
+toggle.addEventListener('click',()=>{
+    body.classList.toggle('dark-mode')
+})
